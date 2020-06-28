30533f0c8d1a72b442fb519b3c934b830df7670e
rajesh <gaya3rachaa@gmail.com>
2020-06-28 21:49:13 +0530

    supplychain

--- Gaya3/suppluChain.sol
+++ Gaya3/suppluChain.sol
@@ -3,18 +3,17 @@ pragma solidity 0.5.17;
 contract SupplyChain{
     
  address public mfgaddress;
+ address public partnerAddress;
 
  struct manufacturer{
     bytes32 mfgName;
-    bytes32 mfgLocation;
-    address mfgaddress; 
+    bytes32 mfgCountry;
+    
  }
  
-
- mapping(address=>manufacturer) manufacturerDetails;
+mapping(address=>manufacturer) manufacturerDetails;
 address[] public manufacturers;
     
-
 
 struct partner{
 
@@ -22,8 +21,10 @@ struct partner{
     bytes32 partnerLocation;
     bytes32 role;
 }
- mapping(address => partner) partnerDetails;
-  address[] public partners;
+
+mapping(address => partner) partnerDetails;
+address[] public partners;
+
 
 struct product{
     bytes32 proId;
@@ -32,21 +33,12 @@ struct product{
     bytes32[] timeStamp;
     address[] partAddress;
 }
-  mapping(bytes32 => product) public productDetails;  
-  bytes32[] public products;
+
+mapping(bytes32 => product) public productDetails;  
+bytes32[] public products;
   
 
  
- function addManufacturer(address _mfgaddress,bytes32 _mfgName,bytes32 _mfgLocation) public{
-        manufacturerDetails[_mfgaddress].mfgaddress=_mfgaddress;
-        manufacturerDetails[_mfgaddress].mfgName = _mfgName;
-        manufacturerDetails[_mfgaddress].mfgLocation = _mfgLocation;
-         manufacturers.push(_mfgaddress);
-    }
-    
-    function verifyManufacturer(address _mfgaddress) view public returns(bytes32, bytes32){
-        return(manufacturerDetails[_mfgaddress].mfgName, manufacturerDetails[_mfgaddress].mfgLocation);
-    }
 constructor() public {
 	mfgaddress = msg.sender;
 }
@@ -56,37 +48,62 @@ modifier onlymanufacturer () {
   if(msg.sender == mfgaddress) _;
 }
    
-    
-    
- 
-    function addPatner(address _partnerAddress,bytes32 _partnerName,bytes32 _partnerLocation,bytes32 _role) onlymanufacturer public{
+modifier onlypartner () {
+  if(msg.sender == partnerAddress) _;
+}    
+
+
+
+    function addManufacturer(address _mfgaddress,bytes32 mfgName,bytes32 mfgCountry) public{
         
-        partnerDetails[_partnerAddress].partnerName = _partnerName;
-        partnerDetails[_partnerAddress].partnerLocation = _partnerLocation;
-        partnerDetails[_partnerAddress].role = _role;
+        manufacturerDetails[_mfgaddress].mfgName = mfgName;
+        manufacturerDetails[_mfgaddress].mfgCountry = mfgCountry;
+        manufacturers.push(_mfgaddress);
+    }
+    
+    function verifyManufacturer(address _mfgaddress) view public returns(bytes32, bytes32){
+        return(manufacturerDetails[_mfgaddress].mfgName, manufacturerDetails[_mfgaddress].mfgCountry);
+    }
+    
+    function addPatner(address _partnerAddress,bytes32 partnerName,bytes32 partnerLocation,bytes32 role) onlymanufacturer public{
+        
+        partnerDetails[_partnerAddress].partnerName = partnerName;
+        partnerDetails[_partnerAddress].partnerLocation = partnerLocation;
+        partnerDetails[_partnerAddress].role = role;
+        partners.push(_partnerAddress);
+        
+    }
+    
+    function editPartner(address _partnerAddress,bytes32 partnerName,bytes32 partnerLocation,bytes32 role) onlymanufacturer public  {
+	        
+        partnerDetails[_partnerAddress].partnerName = partnerName;
+        partnerDetails[_partnerAddress].partnerLocation = partnerLocation;
+        partnerDetails[_partnerAddress].role = role;
         partners.push(_partnerAddress);
     }
     
-     function verifyPartner(address _partnerAddress) view public returns(bytes32, bytes32,bytes32){
+    function verifyPartner(address _partnerAddress) view public returns(bytes32, bytes32,bytes32){
         return(partnerDetails[_partnerAddress].partnerName, partnerDetails[_partnerAddress].partnerLocation, partnerDetails[_partnerAddress].role);
     }
     
-    function listOfPatners() view public returns (address[] memory) {
-        return partners;
+    function addProduct(bytes32 proId,address[] memory partAddress,bytes32 proName,bytes32[] memory proState,bytes32[] memory timeStamp) onlymanufacturer public{
+        
+        productDetails[proId].proName = proName;
+        productDetails[proId].proState = proState;
+        productDetails[proId].timeStamp = timeStamp;
+        productDetails[proId].partAddress = partAddress;
+        products.push(proId);
     }
     
-        function addProduct(bytes32 _proId,address[] memory _partAddress,bytes32 _proName,bytes32[] memory _proState,bytes32[] memory _timeStamp) public{
+    function updateProduct(bytes32 proId,bytes32[] memory proState,bytes32[] memory timeStamp) onlymanufacturer onlypartner public {
         
-        productDetails[_proId].proName = _proName;
-        productDetails[_proId].proState = _proState;
-        productDetails[_proId].timeStamp = _timeStamp;
-        productDetails[_proId].partAddress = _partAddress;
-        products.push(_proId);
+        productDetails[proId].proState = proState;
+        productDetails[proId].timeStamp = timeStamp;     
+    
+    }
+    
+    function verifyProduc(address _partnerAddress) view public returns(bytes32, bytes32,bytes32){
+        return(partnerDetails[_partnerAddress].partnerName, partnerDetails[_partnerAddress].partnerLocation, partnerDetails[_partnerAddress].role);
     }
  
 }
- 
-
-
- 
-
