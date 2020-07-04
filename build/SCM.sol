3b3228e0d305f05074f5e90656100c0823b23953
Divya Maguluri <divya111188@gmail.com>
2020-07-04 16:14:01 +0530

    updated_code

--- build/SCM.sol
+++ build/SCM.sol
@@ -5,13 +5,15 @@ contract SCM{
          
     address public manufacturerAddress;      /* manufacturer update the partner details*/
     address public PartnerAddress;    //partner updates the product details and status
-    uint256 public count;   // By increamenting the proId value generating the id
-        
-        /* restricting the users or partners to edit or add the details.Only manufacturerAddress can add or edit*/
    
-    constructor () public {
+        
+      /* restricting the users or partners to edit or add the details.Only manufacturerAddress can add or edit*/
+   
+    constructor (address) public {
         manufacturerAddress = msg.sender;
+        
     }
+    
     /*if msg.sender is equals to owner(manufacturer) then able to push the data, if not the below error will pop */
     modifier onlymanufacturer (){
         require(msg.sender == manufacturerAddress,"you can't update or edit the details");
@@ -29,6 +31,7 @@ contract SCM{
         address mfgAddress;  /* mfgAddress means eth address */
     } 
     
+    
     struct partner{
         bytes32 partnerName;
         bytes32 partnerLocation;
@@ -37,7 +40,7 @@ contract SCM{
     }
    
     struct product{
-        bytes32 proId;
+        uint256 proId;
         bytes32 proName;
         bytes32[] proState;
         bytes32[] timeStamp;
@@ -67,45 +70,42 @@ contract SCM{
           
           partnerAddress[_partnerAddress].partnerName = _partnerName;
           partnerAddress[_partnerAddress].partnerLocation = _partnerLocation;
-          partnerAddress[_partnerAddress].partnerAddress = _partnerAddress;
+          partnerAddress[_partnerAddress].partnerAddress = PartnerAddress;
           partnerAddress[_partnerAddress].role = _role;
-          count++;
+          
          
           partnerDetails.push(_partnerAddress);
+          
           }
           
           //Editing multiple partner details by manufacturer
           
      function updatePartnerDetails(bytes32 _partnerName,bytes32 _partnerLocation,address _partnerAddress,bytes32 _role) public onlymanufacturer  {
-          require (_partnerAddress == _partnerAddress); //if partnerAddress is equal to the updatePartnerDetails then allow to make the changes.
-            
+         
           partnerAddress[_partnerAddress].partnerName = _partnerName;
           partnerAddress[_partnerAddress].partnerLocation = _partnerLocation;
           partnerAddress[_partnerAddress].partnerAddress = _partnerAddress;
           partnerAddress[_partnerAddress].role = _role;
 
           partnerDetails.pop();
+          
           }
              
            //Partner details will be displaied based on partnerAddressAddress
-            
+           
      function getAllPartnerDetails() view public returns(address [] memory ) {
          return partnerDetails;
+         
           }
-          
-          //List of partners 
-          
-     function noOfPartnerDetails() view public returns(uint256){
-         return partnerDetails.length;
-          }
+   
           // Adding the products
          
-         mapping(bytes32 => product) public proId;
-         bytes32[] public proDetails;
+         mapping(uint256 => product) public proId;
+         uint256 [] public proDetails;
          
      //Adding products screen ,battery,motherboard details by manufacturer or partner
 
-     function addProduct(bytes32 _proId,bytes32 _proName,bytes32 [] memory _proState,bytes32 [] memory _timeStamp,address [] memory _partAddress) public {
+     function addProduct(uint256 _proId,bytes32 _proName,bytes32 [] memory _proState,bytes32 [] memory _timeStamp,address [] memory _partAddress) public {
        require (msg.sender == PartnerAddress || msg.sender == manufacturerAddress); //Only partner and manufacturer are allowed to add the details.
           proId[_proId].proId = _proId;
           proId[_proId].proName = _proName;
@@ -116,24 +116,20 @@ contract SCM{
          }
          
          
-     function updateProduct(bytes32 _proId,bytes32 _proName,bytes32 [] memory _proState,bytes32 [] memory _timeStamp,address [] memory _partAddress)public onlypartner {
+     function updateProduct(uint256 _proId,bytes32 [] memory _proState,bytes32 [] memory _timeStamp)public onlypartner {
       
          require (msg.sender == PartnerAddress || msg.sender == manufacturerAddress);  //Only partner or manufacturer are allowed to make the changes.
          require(_proId > 0);
-          proId[_proId].proId = _proId;
-          proId[_proId].proName = _proName;
+   
           proId[_proId].proState = _proState;
           proId[_proId].timeStamp = _timeStamp;
-          proId[_proId].partAddress = _partAddress;
-         
+        
           proDetails.pop();
          }
-             //Product details will be displaied based on proId
+        //Product details will be displaied based on proId
             
-     function getAllProductDetails() view public returns(bytes32 [] memory ) {
+     function getAllProductDetails() view public returns(uint256 [] memory ) {
          return proDetails;
           }
-          
-     
-     
+    
 }
