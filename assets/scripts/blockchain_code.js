6abb79f527070e9d3bacf402c99dc92346838a2f
Divya Maguluri <divya111188@gmail.com>
2020-07-13 15:32:46 +0530

    added_abi_contractAddress

--- assets/scripts/blockchain_code.js
+++ assets/scripts/blockchain_code.js
@@ -1,5 +1,441 @@
-var contractAddress = "";
-var abi = 
+var contractAddress = "[0x6a97d3e2c6f7831da3ebebbf0321648b6e9e83a9]";
+var abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "_mfgName",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "_mfgLocation",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "address",
+				"name": "_mfgAddress",
+				"type": "address"
+			}
+		],
+		"name": "addManufacturer",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "_partnerName",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "_partnerLocation",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "address",
+				"name": "_partnerAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "_role",
+				"type": "bytes32"
+			}
+		],
+		"name": "addPartner",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "_proName",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "bytes32[]",
+				"name": "_proState",
+				"type": "bytes32[]"
+			},
+			{
+				"internalType": "bytes32[]",
+				"name": "_timeStamp",
+				"type": "bytes32[]"
+			},
+			{
+				"internalType": "bytes32[]",
+				"name": "_stage",
+				"type": "bytes32[]"
+			},
+			{
+				"internalType": "address[]",
+				"name": "_partAddress",
+				"type": "address[]"
+			}
+		],
+		"name": "addProduct",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "_partnerName",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "_partnerLocation",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "address",
+				"name": "_partnerAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "_role",
+				"type": "bytes32"
+			}
+		],
+		"name": "updatePartnerDetails",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_proId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bytes32[]",
+				"name": "_proState",
+				"type": "bytes32[]"
+			},
+			{
+				"internalType": "bytes32[]",
+				"name": "_stage",
+				"type": "bytes32[]"
+			},
+			{
+				"internalType": "bytes32[]",
+				"name": "_timeStamp",
+				"type": "bytes32[]"
+			}
+		],
+		"name": "updateProduct",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "manufacturerAddress",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "mfgAddress",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "mfgName",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "mfgLocation",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "address",
+				"name": "mfgAddress",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "mfgDetails",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "partnerAddress",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "partnerName",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "partnerLocation",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "address",
+				"name": "partnerAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "role",
+				"type": "bytes32"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "partnerDetails",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "proDetails",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "Products",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "proName",
+				"type": "bytes32"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "proId",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_mfgAddress",
+				"type": "address"
+			}
+		],
+		"name": "verifyManufacturer",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "_mfgName",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "_mfgLocation",
+				"type": "bytes32"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_partnerAddress",
+				"type": "address"
+			}
+		],
+		"name": "verifyPartner",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "_partnerName",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "_partnerLocation",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "_role",
+				"type": "bytes32"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_proId",
+				"type": "uint256"
+			}
+		],
+		"name": "verifyProduct",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "_proName",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "bytes32[]",
+				"name": "_proState",
+				"type": "bytes32[]"
+			},
+			{
+				"internalType": "bytes32[]",
+				"name": "_timeStamp",
+				"type": "bytes32[]"
+			},
+			{
+				"internalType": "bytes32[]",
+				"name": "_stage",
+				"type": "bytes32[]"
+			},
+			{
+				"internalType": "address[]",
+				"name": "_partAddress",
+				"type": "address[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
 
 console.log(contractAddress);
 
