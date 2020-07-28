9611350e94f1b1ac1a924f080679b10c03fa50a1
rajesh <gaya3rachaa@gmail.com>
2020-07-28 18:18:24 +0530

    addUser

--- Gaya3/supplyChain.js
+++ Gaya3/supplyChain.js
@@ -1,14 +1,15 @@
-var  contractAddress = "0x38e10ad3634f1a243c96e05bc2b04393e8d36854";
+var  contractAddress = "0x021c8b35f6a72ab88bf97b5e8011f5b2ee48a548";
 var accounts;
 abi = [
 	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
 		"constant": false,
 		"inputs": [
-			{
-				"internalType": "address",
-				"name": "mfgaddress",
-				"type": "address"
-			},
 			{
 				"internalType": "bytes32",
 				"name": "mfgName",
@@ -60,11 +61,6 @@ abi = [
 		"constant": false,
 		"inputs": [
 			{
-				"internalType": "bytes32",
-				"name": "proId",
-				"type": "bytes32"
-			},
-			{
 				"internalType": "address[]",
 				"name": "partAddress",
 				"type": "address[]"
@@ -96,7 +92,7 @@ abi = [
 		"inputs": [
 			{
 				"internalType": "address",
-				"name": "partnerAddress",
+				"name": "_partnerAddress",
 				"type": "address"
 			},
 			{
@@ -122,45 +118,65 @@ abi = [
 		"type": "function"
 	},
 	{
-		"inputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"internalType": "bytes32",
-				"name": "proId",
-				"type": "bytes32"
-			},
-			{
-				"internalType": "bytes32[]",
-				"name": "proState",
-				"type": "bytes32[]"
-			},
-			{
-				"internalType": "bytes32[]",
-				"name": "timeStamp",
-				"type": "bytes32[]"
-			}
-		],
-		"name": "updateProduct",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
 		"constant": true,
 		"inputs": [],
-		"name": "manufacturerAddress",
+		"name": "owner",
 		"outputs": [
 			{
 				"internalType": "address",
 				"name": "",
 				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "partnerDetails",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "partnerAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "partnerName",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "partnerLocation",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "role",
+				"type": "bytes32"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "proId",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
 			}
 		],
 		"payable": false,
@@ -174,72 +190,10 @@ abi = [
 				"internalType": "uint256",
 				"name": "",
 				"type": "uint256"
-			}
-		],
-		"name": "manufacturers",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "partners",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "partnersAddress",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"internalType": "bytes32",
-				"name": "",
-				"type": "bytes32"
 			}
 		],
 		"name": "productDetails",
 		"outputs": [
-			{
-				"internalType": "bytes32",
-				"name": "proId",
-				"type": "bytes32"
-			},
 			{
 				"internalType": "bytes32",
 				"name": "proName",
@@ -262,9 +216,9 @@ abi = [
 		"name": "products",
 		"outputs": [
 			{
-				"internalType": "bytes32",
+				"internalType": "uint256",
 				"name": "",
-				"type": "bytes32"
+				"type": "uint256"
 			}
 		],
 		"payable": false,
@@ -272,16 +226,45 @@ abi = [
 		"type": "function"
 	},
 	{
-		"constant": true,
+		"constant": false,
 		"inputs": [
 			{
-				"internalType": "address",
-				"name": "mfgaddress",
-				"type": "address"
+				"internalType": "uint256",
+				"name": "_proId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address[]",
+				"name": "partAddress",
+				"type": "address[]"
+			},
+			{
+				"internalType": "bytes32[]",
+				"name": "proState",
+				"type": "bytes32[]"
+			},
+			{
+				"internalType": "bytes32[]",
+				"name": "timeStamp",
+				"type": "bytes32[]"
 			}
 		],
+		"name": "updateProduct",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
 		"name": "verifyManufacturer",
 		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
 			{
 				"internalType": "bytes32",
 				"name": "",
@@ -332,9 +315,9 @@ abi = [
 		"constant": true,
 		"inputs": [
 			{
-				"internalType": "bytes32",
-				"name": "proId",
-				"type": "bytes32"
+				"internalType": "uint256",
+				"name": "_proId",
+				"type": "uint256"
 			}
 		],
 		"name": "verifyProduct",
@@ -364,7 +347,7 @@ abi = [
 		"stateMutability": "view",
 		"type": "function"
 	}
-];
+]
 
 window.addEventListener('load',async()=>{
 
@@ -399,12 +382,42 @@ window.addEventListener('load',async()=>{
 
 async function addUser() {
 	var userName = $('#userName').val();
-	var location = $('#location').val();
-	var ethAddress = $('#ethAddress').val();
-	var status = $('#role').val();
-	console.log(userName,location,ethAddress,status);
+	var location = $('#userLocation').val();
+	var ethAddress = $('#userAddress').val();
+	var role = $('#userRole').val();
+	var noOfUsers =0;
+	var userId=345678;
+	console.log(userName,location,ethAddress,role);
 	console.log("iam a issue function");
-	SupplyChain.methods.addPatner()
-	
+	SupplyChain.methods
+		.addPatner(ethAddress,web3.utils.fromAscii(userName),web3.utils.fromAscii(location),web3.utils.fromAscii(role))
+		.send({from: web3.eth.defaultAccount})
+		 .on('receipt',function(receipt){
+			 console.log(receipt);
+		 });
+	// var userData=`<table class="align-middle mb-0 table table-borderless table-striped table-hover">
+	// <thead>
+	// <tr>
+	// 	<th class="text-center">User ID</th>
+	// 	<th>Name</th>
+	// 	<th class="text-center">Location</th>
+	// 	<th class="text-center">ETH Address</th>
+	// 	<th class="text-center">Status</th>
+	// 	<th class="text-center">Actions</th>
+	// </tr>
+	// </thead>
+	// <tbody>`
+	// for(var i=0;i<noOfUsers.length;i++){
+	// 	userData+='<tr><td class="text-center text-muted">'+userId+'</td><td><div class="widget-content p-0"><div class="widget-content-wrapper"><div class="widget-content-left flex2"><div class="widget-heading">John Doe</div>'+userName+
+	// 	'</div></div></div></td><td class="text-center">'+location+
+	// 	'</td><td class="text-center">'+ethAddress+
+	// 	'</td><td class="text-center"><div class="badge badge-warning">'++
+	// 	'</div></td><td class="text-center"><div role="group" class="btn-group-sm btn-group"><button type="button"  class="btn mr-2 mb-2 btn-primary" data-toggle="modal" data-target="#exampleModal">view / update</button></div></td></tr>'
+	// }
+	// userId++;
+	// noOfUsers++;
+	// userData+=`</tbody></table>`
+	// $('#userDetails').append(userData);
+
 }
 
